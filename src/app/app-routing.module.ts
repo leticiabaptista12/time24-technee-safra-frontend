dc0ea1e5d696b900513019fad8c7d20e95b0b274
Lucas Calzavara <lucascalzavara95@gmail.com>
2020-09-11 22:41:29 -0300

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ProductComponent } from './pages/product/product.component';
+import { UserListComponent } from './pages/user-list/user-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: UserListComponent
+  },
+  {
+    path: 'product',
+    component: ProductComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
